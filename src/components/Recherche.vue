1da8ae25335e3f0875a386573cf1b01636591596
Hélène Lessieux <helene@macbook-pro-de-helene.home>
2023-11-22 23:06:30 +0100

    fin du custom de la recherche et du coeur de redirection pour les favoris

--- src/components/Recherche.vue
+++ src/components/Recherche.vue
@@ -1,13 +1,18 @@
 <template>
-  <div>
-    <input type="search" v-model="recherche" placeholder="entrer une ville"/>
-    <button @click="valider">Valider</button>
+  <div class="recherche">
+    <div class="rectangle-de-recherche">
+      <input type="search" v-model="recherche" placeholder="Entrer une ville" class="champ-de-recherche"/>
+      <button @click="valider" class="bouton-de-recherche"><Recherche/></button>
+    </div>
+    <router-link to="/favoris"><IconCoeurBleu/></router-link>
   </div>
 </template>
 
 <script setup lang="ts">
 import {ref} from "vue";
 import {useRouter} from "vue-router";
+import IconCoeurBleu from "@/components/icons/IconCoeurBleu.vue";
+import Recherche from "@/components/icons/Recherche.vue";
 
 const recherche = ref("");
 const router = useRouter();
@@ -22,5 +27,31 @@ function valider() {
 </script>
 
 <style scoped>
+.recherche {
+  display: flex;
+  align-items: center;
+  gap: 30px;
+}
 
+.rectangle-de-recherche {
+  display: flex;
+  align-items: center;
+  background-color: #4DE3ED;
+  border-radius: 24px;
+  padding: 6px 12px;
+}
+
+.champ-de-recherche {
+  background-color: transparent;
+  border: none;
+}
+
+input::placeholder {
+  color: white;
+}
+
+.bouton-de-recherche {
+  background-color: transparent;
+  border: none;
+}
 </style>
